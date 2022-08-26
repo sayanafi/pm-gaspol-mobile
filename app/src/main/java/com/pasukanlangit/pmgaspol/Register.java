32ce14623353eb0187e002c88a188b5d3f6e7b7b
GummyBearz <75606963+Syahfril@users.noreply.github.com>
2022-08-26 17:03:49 +0700

    login regis

---
+++ app/src/main/java/com/pasukanlangit/pmgaspol/Register.java
@@ -0,0 +1,13 @@
+package com.pasukanlangit.pmgaspol;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
--- app/src/main/java/com/pasukanlangit/pmgaspol/login.java
+++ app/src/main/java/com/pasukanlangit/pmgaspol/login.java
@@ -2,7 +2,9 @@ package com.pasukanlangit.pmgaspol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.ButtonRegister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login.this, Register.class));
+            }
+        });
     }
+
+
 }
