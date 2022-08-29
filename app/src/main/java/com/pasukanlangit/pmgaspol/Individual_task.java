2fd3ffd24d7ecea3d9224a6d1af91e20b68ade23
GummyBearz <75606963+Syahfril@users.noreply.github.com>
2022-08-29 14:34:27 +0700

    rought layout

---
+++ app/src/main/java/com/pasukanlangit/pmgaspol/Individual_task.java
@@ -0,0 +1,14 @@
+package com.pasukanlangit.pmgaspol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Individual_task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_task);
+    }
+}
