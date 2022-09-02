3e790efd6619500dff67109053e5a4c93e827b7e
Muhammad Nafi Udin <sayanafime@gmail.com>
2022-09-02 15:43:57 +0700

    update UI & library
    login, register, dashboard & library chart

--- app/src/main/java/com/pasukanlangit/pmgaspol/Dashboard.java
+++ app/src/main/java/com/pasukanlangit/pmgaspol/Dashboard.java
@@ -2,13 +2,114 @@ package com.pasukanlangit.pmgaspol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
+
+import java.util.ArrayList;
+
 
 public class Dashboard extends AppCompatActivity {
+
+    private PieChart pieChart, legend;
+    //private Legend legend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_dashboard);
+
+        //legend = findViewById(R.id.chartlegend);
+        pieChart = findViewById(R.id.chartoverview);
+        setupPieChart();
+        loadPieData();
     }
+
+    private void loadLegend() {
+
+    }
+
+    private void setupPieChart() {
+        pieChart.setDrawHoleEnabled(true);
+//        pieChart.setUsePercentValues(false);
+        pieChart.setCenterText("12");
+//        pieChart.setEntryLabelTextSize(12);
+//        pieChart.setEntryLabelColor(Color.WHITE);
+        pieChart.setCenterTextSize(24);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setDrawEntryLabels(false);
+
+        Legend l = pieChart.getLegend();
+        l.setForm(Legend.LegendForm.CIRCLE);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(true);
+        l.setEnabled(false);
+    }
+
+    //LOAD DATA TOTAL PROJECT YANG AKTIF SETIAP TEAM
+    private void loadPieData() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(6,"Dewabiz"));
+        entries.add(new PieEntry(4,"Iosys"));
+        entries.add(new PieEntry(2,"Cashplus"));
+        entries.add(new PieEntry(1,"ST24"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color: ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color);
+        }
+
+        for (int color: ColorTemplate.VORDIPLOM_COLORS) {
+            colors.add(color);
+        }
+
+        //TOTAL PROJECT SEMUA TEAM
+        PieDataSet dataSet = new PieDataSet(entries, "12");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
+
+    }
+
 }
--- app/src/main/java/com/pasukanlangit/pmgaspol/Register.java
+++ app/src/main/java/com/pasukanlangit/pmgaspol/Register.java
@@ -1,13 +1,27 @@
 package com.pasukanlangit.pmgaspol;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
+
+        findViewById(R.id.backbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Register.this, Login.class));
+            }
+        });
+
     }
+
 }
